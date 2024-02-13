f899e9ac8cff96e19704837141d2cd442c60b228
Matthew <matthew.noojin@hotmail.com>
2024-02-12 17:36:50 -0800

    All changes in my workflow file to include steps in Task 2

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,44 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status 
+
+class CounterTest(TestCase):
+    """Counter tests"""
+
+def test_create_a_counter(self):
+  """It should create a counter"""
+  client = app.test_client()
+  result = client.post('/counters/foo')
+  self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+
+def test_duplicate_a_counter(self):
+  """It should return an error for duplicates"""
+  result = self.client.post('/counters/bar')
+  self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+  result = self.client.post('/counters/bar')
+  self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+def test_update_a_counter(self):
+  result = self.client.post('/counters/meep')
+  self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+  result2 = self.client.post('/counters/meep')
+  self.assertEqual(result.status_code, status.HTTP_200_OK)
+  self.assertNotEqual(result, result2)
+
+
+def test_read_a_counter(self):
+  result = self.client.post('/counters/beep')
+  self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+  result = self.client.post('/counters/beep')
+  self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+def setUp(self):
+  self.client = app.test_client()
