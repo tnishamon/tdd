b19faedd20b505de75c147a5d734a94932af67de
Matthew <matthew.noojin@hotmail.com>
2024-02-12 18:05:59 -0800

    Testing flake8

--- src/counter.py
+++ src/counter.py
@@ -3,8 +3,6 @@ import status
 
 app = Flask(__name__)
 
-
-
 COUNTERS = {}
 
 # We will use the app decorator and create a route called slash counters.
@@ -19,12 +17,12 @@ def create_counter(name):
     app.logger.info(f"Request to create counter:  {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
-@app.route('/counters/<name>', methods=['PUT'])
+@app.route('/counters/<name>',  methods=['PUT'])
 def update_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to update counter: {name}")
@@ -32,10 +30,9 @@ def update_counter(name):
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
-@app.route('/counters/<name>', methods=['GET'])
+@app.route('/counters/<name>',  methods=['GET'])
 def read_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to read counter:  {name}")
     global COUNTERS
     return {name: COUNTERS[name]}, status.HTTP_200_OK
-
